5f960a112bd027294c2213f7c54aad9ed8a6d200
Takumi Sase <78961824+takumi-sase@users.noreply.github.com>
2025-12-05 18:56:14 +0800

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,9 @@
 public class Welcome{
     public static void main(String[] args){
-        System.out.println("EOP is fun (^_^)");
 
-	System.out.println("hello");
+    }
+
+    public static void methodA(){
+        System.out.println("hello");
     }
 }
